3f72b081e69ac58dd2b00187eac710a18042cbf7
Ayan1089 <ayanbanerjee1089@gmail.com>
2023-05-01 09:46:55 +0200

    **Added init files

---
+++ eventseries/matcher.py
@@ -0,0 +1,16 @@
+'''
+Created on 2023-05-01
+
+'''
+from copyreg import constructor
+
+
+class Matcher:
+    """
+    match workshop with event
+    """
+
+    def __init__(self):
+        """
+        constructor
+        """
---
+++ tests/test_matcher.py
@@ -0,0 +1,34 @@
+'''
+Created on 2023-05-01
+
+@author: Ayan
+'''
+import unittest
+from eventseries.matcher import Matcher
+
+class TestMatcher(unittest.TestCase):
+    """
+    test matching
+    """
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testMatcher(self):
+        """
+        test matcher
+        """
+        matcher=Matcher()
+        self.assertTrue(matcher is not None)
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
