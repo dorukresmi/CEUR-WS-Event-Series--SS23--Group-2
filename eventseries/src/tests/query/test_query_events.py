6111582b40fdd160b4b663ab919840722d559c2e
Ayan Banerjee <33069606+Ayan1089@users.noreply.github.com>
2023-06-30 11:26:34 +0200

    "Refactored test cases"

--- eventseries/src/tests/query/test_query_events.py
+++ eventseries/src/tests/query/test_query_events.py
@@ -7,17 +7,14 @@ import os
 import unittest
 
 from eventseries.src.main.query.queried_events import Events
+from pathlib import Path
 
 
 class QueryEvents(unittest.TestCase):
     def test_query_events(self):
         events = Events()
-        events.event_series_query()
-        resources_path = os.path.abspath("resources")
-        event_series_path = os.path.join(resources_path, "event_series.json")
-        self.assertTrue(os.path.exists(event_series_path), "File does not exist at the specified location")
-        # Check if the path corresponds to a file (not a directory)
-        self.assertTrue(os.path.isfile(event_series_path), "Path does not correspond to a file")
+        events_dict = events.read_as_dict()
+        self.assertTrue(bool(events_dict), "Dictionary is empty")
 
 
 if __name__ == "__main__":
